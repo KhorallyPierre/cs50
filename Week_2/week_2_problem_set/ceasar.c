84457f10c6995f1bb3beb0ae3bbd44c072ce7cd6
Khorally-s <kvpierre@gmail.com>
2024-02-16 15:36:07 -0500

    working on passing in full string

--- Week_2/week_2_problem_set/ceasar.c
+++ Week_2/week_2_problem_set/ceasar.c
@@ -10,7 +10,7 @@
 //  Lowercase letters remain lowercase, and uppercase letters remain uppercase.
 //  if no key is provided,keep promtping the user for it, if the key isn't a number, promt the usr for it again
 //  if more than one key is given, promp;t the user tp enter key aain/
-
+char rotate(char character_from_plain_text, int string_key);
 bool only_digits(string string_key);
 int main(int argc, string argv[])
 {
@@ -22,31 +22,30 @@ int main(int argc, string argv[])
 
     string string_key = argv[1];
     // calling digits function when key is inserted
-    only_digits(string_key);
+    int digit_key = only_digits(string_key);
 
     int length = strlen(string_key);
-    if (length > 1)
-    {
-        printf("your key is too long\n");
-        return 1;
-    }
 
-    // convert string to a number
-    int number_key = 0;
-    for (int i = 0; string_key[i] != '\0'; i++)
-    {
-        number_key = number_key * 10 + (string_key[i] - 48);
-    }
-
-    printf("ciphertect: "); ciphertext(plain_text);
     for (int i = 0; i < length; i++)
     {
-        if (isdigit(string_key[i]))
+        if (length > 1 && isdigit(string_key[i]))
         {
-            printf("success \n");
-            string plain_text = get_string("plaintext: ");
+            printf("your key is too long\n");
+            return 1;
         }
     }
+    char current_character;
+    printf("success \n");
+    string plain_text = get_string("plaintext: ");
+    char length_of_plain_text = strlen(plain_text);
+    for (int j = 0; j < length_of_plain_text; j++)
+    {
+        current_character = plain_text[j];
+    }
+    // printf("current character %c \n", current_character);
+    rotate(current_character, digit_key);
+    printf("current character %c \n", current_character);
+    
 }
 
 bool only_digits(string string_key)
@@ -63,6 +62,38 @@ bool only_digits(string string_key)
     }
     return true;
 }
+
+char rotate(char character_from_plain_text, int digit_key)
+{
+    char alphabet_lower[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 's', 't', 'u', 'v', 'x', 'y', 'z'};
+    // printf("characer from plain text %c", character_from_plain_text);
+    for (int i = 0; i < 27; i++)
+
+    {
+        char cipher_character;
+        char lowercase_character = alphabet_lower[i];
+        char uppercase_character = toupper(alphabet_lower[i]);
+        printf("alphabet lower %c \n", lowercase_character);
+        printf("alphabet upper %c \n", uppercase_character);
+        if (character_from_plain_text == alphabet_lower[i])
+        {
+            cipher_character = alphabet_lower[i + digit_key];
+            return cipher_character;
+        }
+        else if (character_from_plain_text == uppercase_character)
+        {
+            cipher_character = uppercase_character + digit_key;
+        }
+        else if (character_from_plain_text != lowercase_character || character_from_plain_text != uppercase_character)
+        {
+            cipher_character = character_from_plain_text;
+        }
+    }
+    return true;
+}
+// Then, implement a function called, e.g., rotate, that takes a char as input and also an int,
+// and rotates that char by that many positions if it’s a letter (i.e., alphabetical), wrapping around from Z to A (and from z to a) as needed.
+// If the char is not a letter, the function should instead return the same char unchanged.
 
 // ciphertext function takes in number key, applies it to plaintext, and returns cyphertext
 
