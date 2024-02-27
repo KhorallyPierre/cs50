998cc663b4606092f269e5b233c01a23076ed91e
Khorally-s <kvpierre@gmail.com>
2024-02-27 14:34:33 -0500

    handling repeats in key

--- Week_2/week_2_problem_set/substitution.c
+++ Week_2/week_2_problem_set/substitution.c
@@ -25,19 +25,30 @@ int main(int argc, string argv[])
             return 1;
         }
         // cant contain non alphanumeric characters
-        else if (isdigit(key[i])){
+        else if (isdigit(key[i]))
+        {
             printf("numbers are not allowed \n");
-            return 1; 
+            return 1;
         }
-        // has to contain each letter only once
-        // for (int j = 0; j < key_length; j++){
-        //     if (key[j] == key[i]){
-        //         printf("you can't have repeats %c", key[j]);
-               
-                
-        //     }
-        // }
+        int counter = 0;
+        int current_key = key[i];
+        int arr_of_chars_for_key[] = {key[0], key[1], key[2], key[3], key[4], key[5], key[6], key[7], key[8], key[9], key[10], key[11], key[12], key[13], key[14], key[15], key[16], key[17], key[18], key[19], key[20], key[21], key[22], key[23], key[24], key[25]};
+
+        for (int i = 0; i < 25; i++)
+        {
+            int each_character = arr_of_chars_for_key[i];
+            if (current_key == each_character)
+            {
+                counter++;
+                if (counter > 2)
+                {
+                    printf("no repeats allowed \n");
+                    return 1;
+                }
+            }
+        }
     }
 }
+
 
 // cc substitution.c -lcs50 -lm -o substitution
