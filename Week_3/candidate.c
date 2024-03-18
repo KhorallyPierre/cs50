b226ff18870091d31abb58a42873175d98e51fcc
Khorally-s <kvpierre@gmail.com>
2024-03-18 15:39:35 -0400

    structs and functions

---
+++ Week_3/candidate.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <cs50.h>
+
+typedef struct
+{
+    string name;
+    int votes;
+} candidate;
+
+candidate get_candidate(string prompt);
+
+int main(void)
+{
+    candidate president = get_candidate("Enter a candidate:");
+    printf("%s \n", president.name);
+    printf("%i\n", president.votes);
+}
+
+candidate get_candidate(string prompt)
+{
+    printf("%s\n", prompt);
+    candidate c;
+    c.name = get_string("Enter a name: ");
+    c.votes = get_int("Enter a number of votes: ");
+    return c;
+}
