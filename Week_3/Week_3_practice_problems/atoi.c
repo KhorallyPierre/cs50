2ba80eb1da60ca2dc1ce36a370eb6a84d3e4896a
Khorally-s <kvpierre@gmail.com>
2024-03-26 22:23:52 -0400

    refactor

--- Week_3/Week_3_practice_problems/atoi.c
+++ Week_3/Week_3_practice_problems/atoi.c
@@ -31,21 +31,17 @@ int convert(string input)
     int input_length = strlen(input);
     char last_element = input[input_length - 1];
     // printf("last element char %c \n", last_element);
-    // to convert a character number to a int number, 
-    //subtract ASCII value from '0'. Same as using atoi. 
+    // to convert a character number to a int number,
+    // subtract ASCII value from '0'. Same as using atoi.
     int last_number = last_element - '0';
-    printf("string %s\n", input);
-    if (input[0] == '\n')
-    {
-        memmove(input, input+1, strlen(input));
-        
-    }
-    int values = atoi(input);
-    printf ("values %i \n", values);
-    
-    // printf("to be removed %c\n", first_number_removal);
+    char first_element = input[0];
+    memmove(input, input + 1, strlen(input));
 
-   
+    // printf("values %i \n", values);
+    printf(" %c", first_element);
+    int first_number = first_element - '0';
+    printf("int values %i \n", first_number);
+
     input[input_length - 1] = '\0';
     // add null after next to last number in string
 
