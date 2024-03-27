340ea8171262e2552b7dcb41a7f39a74bb48221f
Khorally-s <kvpierre@gmail.com>
2024-03-26 21:45:54 -0400

    int

--- Week_3/Week_3_practice_problems/atoi.c
+++ Week_3/Week_3_practice_problems/atoi.c
@@ -28,13 +28,24 @@ int convert(string input)
     // TODO
     // use recursion to print out the given string as an integer
 
-    //  start with the last char and convert it into an integer value.
-    // Then shorten the string, removing the last char,
-    // and then recursively call convert using the shortened string as input,
-    // where the next char will be processed.
     int input_length = strlen(input);
     char last_element = input[input_length - 1];
-    printf("last element %c \n", last_element);
+    // printf("last element char %c \n", last_element);
+    // to convert a character number to a int number, 
+    //subtract ASCII value from '0'. Same as using atoi. 
+    int last_number = last_element - '0';
+    printf("string %s\n", input);
+    if (input[0] == '\n')
+    {
+        memmove(input, input+1, strlen(input));
+        
+    }
+    int values = atoi(input);
+    printf ("values %i \n", values);
+    
+    // printf("to be removed %c\n", first_number_removal);
+
+   
     input[input_length - 1] = '\0';
     // add null after next to last number in string
 
