2d22c0cf6758bd0a2cfdc8380d0fb376b80f9d2e
Khorally-s <kvpierre@gmail.com>
2024-04-03 22:40:05 -0400

    passing arrays to functions

--- Week_3/Week_3_practice_problems/max.c
+++ Week_3/Week_3_practice_problems/max.c
@@ -1,9 +1,4 @@
 // Practice writing a function to find a max value
-//The main function initializes the array, asks the user to enter values,
-//and then passes the array and the number of items to the max function. 
-//Complete the max function by iterating through every element in the array, and return the maximum value.
-
-
 #include <cs50.h>
 #include <stdio.h>
 
@@ -11,12 +6,12 @@ int max(int array[], int n);
 
 int main(void)
 {
+    // The main function initializes the array, asks the user to enter values,
     int n;
     do
     {
         n = get_int("Number of elements: ");
-    } 
-    while (n < 1);
+    } while (n < 1);
 
     int arr[n];
 
@@ -24,12 +19,39 @@ int main(void)
     {
         arr[i] = get_int("Element %i: ", i);
     }
-
+    // and then passes the array and the number of items to the max function.
     printf("The max value is %i.\n", max(arr, n));
 }
 
 // TODO: return the max value
 int max(int array[], int n)
 {
-    return 0;
+    // Complete the max function by iterating through every element in the array, and return the maximum value.
+    // loop through array of elements in arr[n] (i <n)
+  
+    int current;
+    for (int i = 0; i < n; i++)
+    {
+        current = array[i];
+        int current_index = i;
+        printf("current %i \n", current);
+        int next = array[i + 1];
+        int next_index = i+1;
+        printf("next %i \n", next);
+
+        if (array[i] > array[i + 1])
+        {
+            //current index becomes next index
+            // left goes to right's index
+            array[i +1] = current;
+            // right goes to left's idnex
+            array[i] = next;
+            printf("current %i \n", current);
+
+            // compare next index to the one after it 
+            
+        }
+    }
+   
+    return current;
 }
